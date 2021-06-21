8d52aead40cf8831200e00a2087956e18d6a334b
Francisco Ibarra <silentium77@hotmail.com>
2021-06-20 19:43:41 -0500

    Handle errors and status

--- toy-blocks-test/src/containers/Blocks.js
+++ toy-blocks-test/src/containers/Blocks.js
@@ -4,14 +4,29 @@ import Block from "../components/Block";
 
 const Blocks = ({blocks}) => {
   const classes = useStyles()
-  return (
-    <Grid container className={classes.blockContainer}>
-      { blocks.list.map( item => (
-        <Grid item md={12} key={item.id}>
-          <Block block={item}></Block>
+  return ( <React.Fragment>
+      {
+        blocks.isLoading && <Grid container className={classes.blockContainer}>
+          <Grid item md={12}>
+              <Typography>Is loading...</Typography>
+          </Grid>
+          </Grid>
+      }
+    { !blocks.isLoading && !blocks.error && <Grid container className={classes.blockContainer}>
+        { blocks.list.map( item => (
+          <Grid item md={12} key={item.id}>
+            <Block block={item}></Block>
+          </Grid>
+        ))}
+      </Grid>
+    }
+      { !blocks.isLoading && blocks.error && <Grid container className={classes.blockContainer}>
+        <Grid item md={12}>
+          <Typography>{blocks.error}</Typography>
         </Grid>
-      ))}
-    </Grid>
+        </Grid>
+      }
+    </React.Fragment>
   );
 };
 
