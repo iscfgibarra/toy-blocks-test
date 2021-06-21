d4b957e9b13049967595576a30e85392e7b1a7d5
Francisco Ibarra <silentium77@hotmail.com>
2021-06-20 19:58:04 -0500

    Last modifications

--- toy-blocks-test/src/containers/Blocks.js
+++ toy-blocks-test/src/containers/Blocks.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import {Box, Grid, makeStyles, Typography} from "@material-ui/core";
+import {Box, CircularProgress, Grid, makeStyles, Typography} from "@material-ui/core";
 import Block from "../components/Block";
 
 const Blocks = ({blocks}) => {
   const classes = useStyles()
   return ( <React.Fragment>
       {
-        blocks.isLoading && <Grid container className={classes.blockContainer}>
-          <Grid item md={12}>
+        blocks.isLoading && <Grid container className={classes.blockContainer} spacing={1}>
+          <Grid item md={12} lg={12} xs={12}  className={classes.itemFlex}>
               <Typography>Is loading...</Typography>
           </Grid>
+          <Grid item md={12} lg={12} xs={12} className={classes.itemFlex}>
+              <CircularProgress />
           </Grid>
+      </Grid>
       }
     { !blocks.isLoading && !blocks.error && <Grid container className={classes.blockContainer}>
         { blocks.list.map( item => (
@@ -22,7 +25,7 @@ const Blocks = ({blocks}) => {
     }
       { !blocks.isLoading && blocks.error && <Grid container className={classes.blockContainer}>
         <Grid item md={12}>
-          <Typography>{blocks.error}</Typography>
+          <Typography className={classes.errorBlocks}>{blocks.error}</Typography>
         </Grid>
         </Grid>
       }
@@ -32,7 +35,22 @@ const Blocks = ({blocks}) => {
 
 const useStyles = makeStyles((theme) => ({
   blockContainer: {
-    margin:  5
+    margin:  "0 10px"
+  },
+  errorBlocks: {
+    color: "white",
+    fontWeight: "bold",
+    fontFamily: "Roboto",
+    backgroundColor: "#ff0932",
+    border: "solid 1px #f98298",
+    width: "95%",
+    borderRadius: "5px",
+    padding: "3px"
+  },
+  itemFlex: {
+    display: "flex",
+    alignItems: "center",
+    justifyItems: "center"
   }
 }));
 
